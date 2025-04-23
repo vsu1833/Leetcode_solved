99fb6a0e83f16abae622f5269d463f8b678155ae
Viraj Upadhyay <vsu1833@gmail.com>
2025-04-23 12:32:40 +0530

    C++ Solution
          Submission Statistics:
          Question #: 485
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 50.14 MB
          Memory percentile: 64.79

---
+++ 485. Max Consecutive Ones/Solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+       int count = 0 ;
+       int maxcount = 0 ;
+       for(int i = 0 ; i < nums.size() ; i++)
+       {
+        if(nums[i]!=1)
+        {
+            count = 0 ;
+        }
+        else
+        { count++;
+            maxcount = max(count, maxcount);
+           
+        } 
+       }
+       return maxcount;
+    }
+};
