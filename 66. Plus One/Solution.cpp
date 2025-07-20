fd55de6559463a6ebab9693eb79ca0b3c5ef9d97
Viraj Upadhyay <virajupadhyay16@gmail.com>
2025-07-20 16:15:56 +0530

    C++ Solution
          Submission Statistics:
          Question #: 66
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 11.584 MB
          Memory percentile: 34.84

---
+++ 66. Plus One/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int index=digits.size()-1;
+    while(index>=0)
+    {
+        if(digits[index]==9){
+            digits[index]=0;
+        }
+        else
+        {
+            digits[index]+=1;
+            return digits;
+        }
+
+        index--;
+    }
+    digits.insert(digits.begin(),1);
+    return digits;
+    }
+};
