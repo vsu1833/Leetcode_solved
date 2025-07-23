78baf9275f45fec360c6d797351e6b38ac0ca662
Viraj Upadhyay <virajupadhyay16@gmail.com>
2025-07-23 23:10:58 +0530

    C++ Solution
          Submission Statistics:
          Question #: 1605
          Language: C++
          Runtime: 19 ms
          Runtime percentile: 82.21
          Memory: 70.476 MB
          Memory percentile: 76.31

---
+++ 1605. Minimum Number of Days to Make m Bouquets/Solution.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool possible(vector<int> &arr, int day, int m, int k) {
+        int n = arr.size();
+        int cnt = 0;
+        int noOfB = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] <= day) {
+                cnt++;
+            }
+            else {
+                noOfB += (cnt / k);
+                cnt = 0;
+            }
+        }
+        noOfB += (cnt / k);
+        return noOfB >= m;
+    }
+    
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        long long val = m * 1ll * k * 1ll;
+        int n = bloomDay.size();
+        if (val > n) return -1;
+        
+        int mini = INT_MAX, maxi = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            mini = min(mini, bloomDay[i]);
+            maxi = max(maxi, bloomDay[i]);
+        }
+
+        int left = mini, right = maxi;
+        int result = -1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (possible(bloomDay, mid, m, k)) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return result;
+    }
+};
