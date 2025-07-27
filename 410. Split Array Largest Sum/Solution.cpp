dd3c8419a1a37a48c120c546d4fafe074696fe39
Viraj Upadhyay <virajupadhyay16@gmail.com>
2025-07-27 23:08:33 +0530

    C++ Solution
          Submission Statistics:
          Question #: 410
          Language: C++
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 10.36 MB
          Memory percentile: 87.16

---
+++ 410. Split Array Largest Sum/Solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int k) {
+        int low = *max_element(nums.begin(), nums.end());
+        int high = accumulate(nums.begin(), nums.end(), 0);
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            int partitions = countPartitions(nums, mid);
+
+            if (partitions > k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+    int countPartitions(vector<int> &a, int maxSum) {
+        int n = a.size();
+        int partitions = 1;
+        long long subarraySum = 0;
+        for (int i = 0; i < n; i++) {
+            if (subarraySum + a[i] <= maxSum) {
+                subarraySum += a[i];
+            } else {
+                partitions++;
+                subarraySum = a[i];
+            }
+        }
+        return partitions;
+    }
+};
