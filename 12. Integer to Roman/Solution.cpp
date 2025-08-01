dedbe2ce5cb48fb6de357973f8a8c5807c00e6e8
Viraj Upadhyay <virajupadhyay16@gmail.com>
2025-08-01 23:41:53 +0530

    C++ Solution
          Submission Statistics:
          Question #: 12
          Language: C++
          Runtime: 3 ms
          Runtime percentile: 63.19
          Memory: 12.172 MB
          Memory percentile: 50.83

---
+++ 12. Integer to Roman/Solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string result = "";
+        vector<int> values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        vector<string> symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        for (int i = 0; i < values.size(); ++i) {
+            while (num >= values[i]) {
+                result += symbols[i];
+                num -= values[i];
+            }
+        }
+
+        return result;
+    }
+};
