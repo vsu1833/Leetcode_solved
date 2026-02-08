feffd422c073dd075b3cd751426029affce9649e
Viraj Upadhyay <stud22.vsu1@gec.ac.in>
2026-02-08 15:26:31 +0530

    [509] [Time Beats: 46.14%] [Memory Beats: 63.91%] - LeetPush

--- Fibonacci-Number.cpp
+++ Fibonacci-Number.cpp
@@ -1,13 +1,10 @@
 1class Solution {
 2public:
 3    int fib(int n) {
-4        if (n <= 1) return n;
-5        vector<int> dp(n+1);
-6        dp[0] = 0;
-7        dp[1] = 1;
-8        for (int i = 2; i <= n; i++) {
-9            dp[i] = dp[i - 1] + dp[i - 2];
-10        }
-11        return dp[n];
-12    }
-13};
+4        if (n == 0) return 0;
+5        if (n == 1) return 1;
+6
+7        return fib(n - 1) + fib(n - 2);
+8    }
+9};
+10
