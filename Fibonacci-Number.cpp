057897a93e5117d42a3ab97b50f0ac6e3924d4a2
Viraj Upadhyay <stud22.vsu1@gec.ac.in>
2026-02-08 15:41:52 +0530

    [509] [Time Beats: 100.00%] [Memory Beats: 35.98%] - LeetPush

--- Fibonacci-Number.cpp
+++ Fibonacci-Number.cpp
@@ -4,7 +4,28 @@
 4        if (n == 0) return 0;
 5        if (n == 1) return 1;
 6
-7        return fib(n - 1) + fib(n - 2);
-8    }
-9};
+7        int fib[n + 1];
+8        fib[0] = 0;
+9        fib[1] = 1;
 10
+11        for (int i = 2; i <= n; i++) {
+12            fib[i] = fib[i - 1] + fib[i - 2];
+13        }
+14
+15        return fib[n];
+16    }
+17};
+18/*
+19
+20class Solution {
+21public:
+22    int fib(int n) {
+23        if (n == 0) return 0;
+24        if (n == 1) return 1;
+25
+26        return fib(n - 1) + fib(n - 2);
+27    }
+28};
+29
+30*/
+31
